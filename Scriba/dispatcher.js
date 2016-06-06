e6ab16700052d22e8b5d2312ab1c0e793b3eda32
Giuseppe Iaffaldano <Giuseppe Iaffaldano>
2016-06-06 10:47:29 +0200

    Il server (dispatcher) è configurato per girare sul server collab. Per farlo girare altrove bisogna cambiare la destinazione della connessione della socket nei client (speakerSide e clientSide)

---
+++ Scriba/dispatcher.js
@@ -0,0 +1,37 @@
+﻿/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */ 
+ 
+
+var app = require("express");
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
+server.listen(48922);
+
+console.log("Server is Running");
+
+io.on('connection', function (socket) {
+    console.log("User Connected");
+    socket.on('client_message',function(data){
+        console.log(data.text);
+        socket.broadcast.emit('server_message',{text:data.text });
+    });
+});
+
+/*
+var io = require('socket.io').listen(48922);
+var http = require('http');
+
+console.log("Server is Running");
+
+io.sockets.on('connection', function (socket) {
+    console.log("User Connected");
+    socket.on('client_message', function (data) {
+        console.log(data.text);
+        io.socket.broadcast.emit('server_message', { text: data.text });
+    });
+});
+*/ 
